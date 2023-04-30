45ca5cc8c6c5e0130dceb9fd0e6cf369da32620e
Jaime Vaquero Rabahieh <95858748+100472248@users.noreply.github.com>
2023-04-30 17:43:01 +0200

    comkienzo del banco

--- ssoo_p3_multihilo_2023/bank.c
+++ ssoo_p3_multihilo_2023/bank.c
@@ -21,6 +21,17 @@
  * @return
  */
 int main (int argc, const char * argv[] ) {
+    /*Como se tienen que meter 5 argumentos, argc debe valer 6 como mínimo.*/
+    if (argc < 6){
+        perror("Número de argumentos insuficientes\n")
+    }
+    /*Antes de empezar, vamos a comprobar que todo funciona correctamente.*/
+    if (argv[2] < 0) || (argv[3] < 0) {
+        perror("Número de hilos negativo")
+    }
+    int fd;
+    fd = open(argv[1], O_RDNLY|O_CREAT|O_TRUNC, 0544);
 
+    close(fd);
     return 0;
 }
